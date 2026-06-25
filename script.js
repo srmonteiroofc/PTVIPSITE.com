4146104d8deaf4dffbf9f3f6b8dc1dd9ac25dc0d
srmonteiroofc <miguelmonteiro.300701@gmail.com>
2026-06-25 17:31:26 -0300

    Add function to copy text to clipboard

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function irPara(pagina) {
     window.location.href = pagina;
 }
 
+function copiarPix() {
+    const texto = document.getElementById('codigoCopiaCola').innerText;
+    navigator.clipboard.writeText(texto).then(() => {
+        const msg = document.getElementById('msgCopiado');
+        msg.style.display = 'block';
+        setTimeout(() => msg.style.display = 'none', 3000);
+    });
+}
