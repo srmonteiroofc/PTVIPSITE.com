203ba8e3c70aac6c0082b5b5f97cfb7fa31c649f
srmonteiroofc <miguelmonteiro.300701@gmail.com>
2026-06-27 16:33:19 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,14 @@
-// firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "SUA_API_KEY",
-authDomain: "ptvip-teste.firebaseapp.com",
-projectId: "ptvip-teste",
-storageBucket: "ptvip-teste.firebasestorage.app",
-messagingSenderId: "407028833766",
-appId: "1:407028833766:web:79d7dbf7ee968944248a27",
-measurementId: "G-1XXWV7S8YH"
+  apiKey: "AIzaSyAw_X2_5ntROLBDHj6TF_rFQj6Yra-ghxk",
+  authDomain: "ptvip-teste.firebaseapp.com",
+  projectId: "ptvip-teste",
+  storageBucket: "ptvip-teste.firebasestorage.app",
+  messagingSenderId: "407028833766",
+  appId: "1:407028833766:web:79d7dbf7ee968944248a27"
 };
 
 const app = initializeApp(firebaseConfig);
-
-getAnalytics(app);
-
-export { app };
+export const auth = getAuth(app); // O segredo está nesta linha
